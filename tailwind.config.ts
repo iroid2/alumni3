a5b50c6a4a231010c43c117803e2f75af9f53652
iroid2 <iradtu2@gmail.com>
2024-09-07 14:18:30 +0300

    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+		center: true,  
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -88,30 +88,30 @@ const config = {
   					height: '0'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+  			// 'accordion-down': {
+  			// 	from: {
+  			// 		height: '0'
+  			// 	},
+  			// 	to: {
+  			// 		height: 'var(--radix-accordion-content-height)'
+  			// 	}
+  			// },
+  			// 'accordion-up': {
+  			// 	from: {
+  			// 		height: 'var(--radix-accordion-content-height)'
+  			// 	},
+  			// 	to: {
+  			// 		height: '0'
+  			// 	}
+  			// }
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
+  	 
   			meteor: 'meteor 5s linear infinite',
   			gradient: 'gradient 8s linear infinite',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			// 'accordion-down': 'accordion-down 0.2s ease-out',
+  			// 'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
