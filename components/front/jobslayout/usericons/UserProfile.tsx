93cc7f6a41b36b789f71827d8c7ed5c9f2f87ae7
iroid <iradtu2@gmail.com>
2024-10-01 11:59:52 +0300

    logou on frontt

--- components/front/jobslayout/usericons/UserProfile.tsx
+++ components/front/jobslayout/usericons/UserProfile.tsx
@@ -1,36 +1,50 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import Image from 'next/image'
 import React from 'react'
+import { useSession, signOut } from 'next-auth/react'
+import Link from 'next/link'
 
 export default function UserProfile() {
-  return (
-    
-    <DropdownMenu>
-    <DropdownMenuTrigger asChild>
-      <Button
-        variant="outline"
-        size="icon"
-        className="overflow-hidden rounded-full"
-      >
-        <Image
-          src="https://img.freepik.com/premium-photo/picture-african-american-man-smiling-passport-photo_1105541-16752.jpg?w=740"
-          width={36}
-          height={36}
-          alt="Avatar"
-          className="overflow-hidden rounded-full"
-        />
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align="end">
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>Settings</DropdownMenuItem>
-      <DropdownMenuItem>Support</DropdownMenuItem>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>Logout</DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+  const { data: session } = useSession()
 
+  if (!session) {
+    return null
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full"
+        >
+          <Image
+            src={session.user?.image || "https://img.freepik.com/premium-photo/picture-african-american-man-smiling-passport-photo_1105541-16752.jpg?w=740"}
+            width={36}
+            height={36}
+            alt="Avatar"
+            className="overflow-hidden rounded-full"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>{session.user?.name || 'My Account'}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/support">Support</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onSelect={() => signOut({ callbackUrl: '/login' })}>
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
